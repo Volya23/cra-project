87c5b475afa9a2db16f3bbf3718632d1416b7ba4
Volya23 <v.o.kryzhanivska@gmail.com>
2023-10-22 01:05:30 +0300

    light

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
-  render() {
-    return(
-      <div className='circle'>
+  constructor(props) {
+    super(props);
+    this.state = {
+      light: true
+    }
+  }
+  toggle () {
+    this.setState({
+      light: !this.state.light
+    })
+  }
 
-      </div>
+  render() {
+    const className = this.state.light ? 'lightOn' : 'lightOff'
+    return(
+      <>
+      <div className={className}></div>
+      <button onClick={() => {this.toggle()}}>Toggle</button>
+      </>
+
     )
   }
 }
