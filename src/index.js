a39a3a163c119d55aff418195b487f3af10f13c5
Volya23 <v.o.kryzhanivska@gmail.com>
2023-10-22 01:28:20 +0300

    Companents + Greeting

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App/App';
+import Greeting from './components/Greeting/Greeting';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <Greeting name= 'Sam'/>
+  <Greeting name= 'Lim'/>
+  <Greeting name= 'Cat'/>
+  <Greeting name= 'Lina'/>
+  <Greeting name= 'Dina'/>
+  </>
 );
