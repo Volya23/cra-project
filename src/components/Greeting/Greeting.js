24c9116daa7880a0537baa5fed89381c7f095b71
Volya23 <v.o.kryzhanivska@gmail.com>
2023-10-22 21:10:39 +0300

    Hi + Bye

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -6,12 +6,21 @@ class Greeting extends React.Component {
         super(props);
         
         this.state = {
-
+            isGreeting: true
         }
     }
 
+    meeting () {
+        this.setState( {
+            isGreeting:  !this.state.isGreeting
+        })
+    } 
+
     render() {
-        return <h1>Hello, {this.props.name}</h1>
+        const {isGreeting} = this.state;
+        const greetText = isGreeting ===true ? 'Hello' : 'Bye';
+        return <h1 onClick={() => {this.meeting()}}>{greetText}, {this.props.name}</h1>
     }
 }
 export default Greeting;
+
