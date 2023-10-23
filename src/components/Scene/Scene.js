1390fe5938eaf627c6b35cfea2fa3bd555f364e7
Volya23 <v.o.kryzhanivska@gmail.com>
2023-10-23 21:30:33 +0300

    Scene

---
+++ src/components/Scene/Scene.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Toggler from './Toggler';
+import './style.css'
+
+class Scene extends React.Component {
+    constructor(props) {
+        super (props)
+
+        this.state = {
+            light: true
+        }
+    }
+
+    toggleScene() {
+        this.setState({
+            light: !this.state.light
+        })
+    }
+
+    render() {
+        const {light} = this.state;
+        return (
+           <div className= {light === true ? 'scene-off' : 'scene-on'}>
+                <Toggler changeParent={() => {this.toggleScene()}}/>
+            </div>
+        )
+    }
+}
+
+export default Scene;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 //import App from './components/App/App';
 //import Meeting from './components/Meeting/MeetingDashboard';
 //import MeetingDashboard from './components/Meeting/MeetingDashboard';
-import ToDoList from './components/ToDoList/ToDoList';
+//import ToDoList from './components/ToDoList/ToDoList';
+import Scene from './components/Scene/Scene';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ToDoList/>
+  <Scene/>
 );
